dc650a83594085fc1a0ffa6484eec18e84e6a9b7
Gabriel Falcão <gabriel@nacaolivre.org>
2010-09-06 15:57:28 -0700

    adding tests to core.Language

--- tests/unit/test_language.py
+++ tests/unit/test_language.py
@@ -28,10 +28,36 @@ def test_language_is_english_by_default():
     assert_equals(lang.scenario, 'Scenario')
     assert_equals(lang.examples, 'Examples|Scenarios')
     assert_equals(lang.scenario_outline, 'Scenario Outline')
+    assert_equals(repr(lang), '<Language "en">')
 
 def test_language_has_first_of():
     "Language() can pick up first occurrece of a string"
     lang = Language()
 
     assert_equals(lang.first_of_examples, 'Examples')
+
+
+def test_language_guess_from_string():
+    "language should be able to guess from string"
+
+    pt_br = """
+
+    # language:           pt-br
+
+    """
+
+    lang_pt_br = Language.guess_from_string(pt_br)
+    assert_equals(lang_pt_br.code, 'pt-br')
+
+def test_language_guess_from_string_falls_back_to_english():
+    "Language.guess_from_string falls back to english if there is no proper language string"
+
+    weird = """
+
+    # languag asdpt-br
+
+    """
+
+    lang_pt_br = Language.guess_from_string(weird)
+    assert_equals(lang_pt_br.code, 'en')
 
